257b0163f38bb3cc374f37ec9222567f8c3aa78c
zhuang.a.ren <zhuang.a.ren@rakuten.com>
2021-08-11 18:04:06 +0800

    Finish the development of Sign In page.

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, Form, Input } from '@alifd/next';
-import { Link } from 'ice';
 import styles from './index.module.scss';
+import { Link } from 'ice';
 
 const FormItem = Form.Item;
 
@@ -9,31 +9,33 @@ const SignIn = () => {
   const handleSubmit = () => {};
 
   return (
-    <Box align="center" justify="center" className={styles.signInWrapper}>
-      <h1>Hello!</h1>
-      <p>Fill in your username and password to sign in.</p>
-      <Form>
-        <FormItem required requiredMessage="Please input your username!">
-          <Input name="username" placeholder="Username" />
-        </FormItem>
-        <FormItem required requiredMessage="Please input your password!">
-          <Input.Password name="password" placeholder="Password" />
-        </FormItem>
-        <FormItem>
-          <Form.Submit type="primary" validate onClick={handleSubmit}>
-            SIGN IN
-          </Form.Submit>
-        </FormItem>
-      </Form>
-      <Link
-        to={{
-          pathname: '/sign-up',
-        }}
-        className={styles.signUpLink}
-      >
-        Don&apos;t have an account? Sign up now!
-      </Link>
-    </Box>
+    <div className={styles.signInWrapper}>
+      <Box className={styles.signInBox}>
+        <h1>Hello!</h1>
+        <p>Fill in your username and password to sign in.</p>
+        <Form>
+          <FormItem required requiredMessage="Please input your username!">
+            <Input name="username" placeholder="Username" />
+          </FormItem>
+          <FormItem required requiredMessage="Please input your password!">
+            <Input.Password name="password" placeholder="Password" />
+          </FormItem>
+          <FormItem>
+            <Form.Submit type="primary" validate onClick={handleSubmit}>
+              SIGN IN
+            </Form.Submit>
+          </FormItem>
+        </Form>
+        <Link
+          to={{
+            pathname: '/sign-up',
+          }}
+          className={styles.signUpLink}
+        >
+          Don&apos;t have an account? Sign up now!
+        </Link>
+      </Box>
+    </div>
   );
 };
 
